290f524d503f69f7919ea3e9659ea97c9da8b981
Jonathan Rodriguez <jonathanrodriguez981207@gmail.com>
2020-02-19 19:53:41 -0500

    Importar matirial

--- prueba/src/app/app.module.ts
+++ prueba/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
